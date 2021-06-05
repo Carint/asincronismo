55c0ae0fedcb578d59707b3ba6f13c3cc84c833a
Carlos Interiano <carinthnd@gmail.com>
2021-06-05 07:18:50 -0600

    Encadenamiento de promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -8,3 +8,24 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(response => console.log(response))
     .catch(err => console.error(err))
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => { resolve('true')}, 2000)
+        } else {
+            // MOSTRARA MAS INFORMACION SOBRE EL ERROR
+            const error = new Error('Whooop!')
+            reject(error)
+        }
+    })
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
+
+// ENCADENAR O EJECUTAR VARIAS PROMESAS
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => console.log('Array of results', response))
+    .catch(err => console.log(err))
